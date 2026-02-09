a86038c33a86fc7067f20269e9f2c454dd7409a0
Ocskó Gábor <ogabor@gmail.com>
2026-02-09 14:59:31 +0100

    Refactor agents and update notebook

--- src/agents.py
+++ src/agents.py
@@ -2,8 +2,14 @@ from langchain_openai import ChatOpenAI
 from langchain.agents import create_agent
 
 
-SYSTEM_PROMPT = """
+SYSTEM_PROMPT_FRONTEND_DEVELOPER = """
 You are a frontend software developer. 
+Anwser the user's question in a friendly and helpful manner.
+
+"""
+
+SYSTEM_PROMPT_BUSINESS_ANALYST = """
+You are a business analyst.
 Anwser the user's question in a friendly and helpful manner.
 
 """
@@ -26,10 +32,21 @@ frontendDeveloperLocal = ChatOpenAI(
 
 frontendDeveloperAgent = create_agent(
     model=frontendDeveloper,
-    system_prompt=SYSTEM_PROMPT,
+    system_prompt=SYSTEM_PROMPT_FRONTEND_DEVELOPER,
 )
 
 frontendDeveloperAgentLocal = create_agent(
     model=frontendDeveloperLocal,
-    system_prompt=SYSTEM_PROMPT,
+    system_prompt=SYSTEM_PROMPT_FRONTEND_DEVELOPER,
+)
+
+businessAnalyst = ChatOpenAI(
+    model="gpt-4o-mini",
+    temperature=0,
+    use_responses_api=True,
+)
+
+businessAnalystAgent = create_agent(
+    model=businessAnalyst,
+    system_prompt=SYSTEM_PROMPT_BUSINESS_ANALYST,
 )
